b0860739f8f6c606cbdb150afabc895ceea1dd1a
Michael Johne <mjohne@users.noreply.github.com>
2018-10-31 23:40:28 +0100

    Add files via upload

--- openForensics/MainForm.Designer.cs
+++ openForensics/MainForm.Designer.cs
@@ -28,22 +28,110 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 29);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 58);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(180, 105);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "label1";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(12, 169);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(80, 17);
+			this.checkBox1.TabIndex = 3;
+			this.checkBox1.Text = "checkBox1";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Location = new System.Drawing.Point(13, 193);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(85, 17);
+			this.radioButton1.TabIndex = 4;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "radioButton1";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(120, 170);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 5;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(120, 198);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+			this.numericUpDown1.TabIndex = 6;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.Name = "MainForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "openForensics";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
 	}
 }
 
--- openForensics/MainForm.cs
+++ openForensics/MainForm.cs
@@ -16,5 +16,42 @@ namespace openForensics
 		{
 			InitializeComponent();
 		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			BackColor = Color.FromArgb(45, 45, 45);
+			ForeColor = Color.FromArgb(241, 241, 241);
+			label1.ForeColor = ForeColor;
+			label1.FlatStyle = FlatStyle.Flat;
+			textBox1.BackColor = Color.FromArgb(30, 30, 30);
+			textBox1.ForeColor = ForeColor;
+			textBox1.BorderStyle = BorderStyle.FixedSingle;
+			button1.ForeColor = ForeColor;
+			button1.FlatStyle = FlatStyle.Flat;
+			button1.FlatAppearance.BorderColor = Color.FromArgb(67, 67, 67);
+			button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(27, 27, 27);
+			button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(62, 62, 62);
+			button1.FlatAppearance.CheckedBackColor = Color.FromArgb(30, 30, 30);
+			checkBox1.ForeColor = ForeColor;
+			checkBox1.BackColor = BackColor;
+			checkBox1.FlatStyle = FlatStyle.Flat;
+			checkBox1.FlatAppearance.BorderColor= Color.FromArgb(67, 67, 67);
+			checkBox1.FlatAppearance.MouseDownBackColor = Color.FromArgb(27, 27, 27);
+			checkBox1.FlatAppearance.MouseOverBackColor = Color.FromArgb(62, 62, 62);
+			checkBox1.FlatAppearance.CheckedBackColor = Color.FromArgb(30, 30, 30);
+			radioButton1.ForeColor = ForeColor;
+			radioButton1.BackColor = BackColor;
+			radioButton1.FlatStyle = FlatStyle.Flat;
+			radioButton1.FlatAppearance.BorderColor = Color.FromArgb(67, 67, 67);
+			radioButton1.FlatAppearance.MouseDownBackColor = Color.FromArgb(27, 27, 27);
+			radioButton1.FlatAppearance.MouseOverBackColor = Color.FromArgb(62, 62, 62);
+			radioButton1.FlatAppearance.CheckedBackColor = Color.FromArgb(30, 30, 30);
+			comboBox1.ForeColor = ForeColor;
+			comboBox1.BackColor = Color.FromArgb(51, 51, 51);
+			comboBox1.FlatStyle = FlatStyle.Flat;
+			numericUpDown1.ForeColor = ForeColor;
+			numericUpDown1.BackColor = Color.FromArgb(51, 51, 51);
+			numericUpDown1.BorderStyle = BorderStyle.FixedSingle;
+		}
 	}
 }
